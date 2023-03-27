2e0a4187c314dcaff7ef7420fb2fc0d245e2baa6
Everlyne <evahaudi2354@gmail.com>
2023-03-27 01:06:45 -0700

    function that prints a string, in reverse, followed by a new line.

--- 0x05-pointers_arrays_strings/_putchar.c
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -7,7 +7,7 @@
  */
 int _putchar(char c)
 {
-	return (write(1, &c, 1));
+        return (write(1, &c, 1));
 }
 
 /**
@@ -16,9 +16,37 @@ int _putchar(char c)
  */
 void _puts(char *str)
 {
-	while (*str != '\0')
-	{
-		_putchar(*str++);
-	}
-	_putchar('\n');
+        while (*str != '\0')
+        {
+                _putchar(*str++);
+        }
+        _putchar('\n');
+}
+
+/**
+ * print_rev - prints a string in reverse order
+ * @s: the string to print
+ * Return: none
+ */
+void print_rev(char *s)
+{
+        /* Calculate the length of the string */
+        int longi = 0;
+        int o;
+
+        while (*s != '\0')
+        {
+                longi++;
+                s++;
+        }
+
+        /* Print the string in reverse order */
+        s--;
+        for (o = longi; o > 0; o--)
+        {
+                _putchar(*s);
+                s--;
+        }
+
+        _putchar('\n'); /* Print a newline character at the end */
 }
