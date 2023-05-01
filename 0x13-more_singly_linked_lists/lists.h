04bf95ec77bc5720d3a69a3bd6b6900cf21d0044
Everlyne <evahaudi2354@gmail.com>
2023-05-01 12:02:04 -0700

    it is a function that returns the sum of all the data (n) of a listint_t linked list.

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -22,6 +22,7 @@ listint_t *add_nodeint(listint_t **head, const int n);
 int pop_listint(listint_t **head);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
+int sum_listint(listint_t *head);
 void free_listint2(listint_t **head);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
