f54546b1ccf7ca81f3357579d950522b744f6016
Everlyne <evahaudi2354@gmail.com>
2023-05-01 12:14:56 -0700

    it is a function that inserts a new node at a given position.

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -20,6 +20,7 @@ typedef struct listint_s
 
 listint_t *add_nodeint(listint_t **head, const int n);
 int pop_listint(listint_t **head);
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int sum_listint(listint_t *head);
