bf8a4adfac84227cf2c607d2833b79a95a357cb0
Everlyne <evahaudi2354@gmail.com>
2023-05-01 04:42:33 -0700

    updated

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,11 +8,12 @@
 size_t print_listint(const listint_t *h)
 {
 	size_t nm = 0;
+
 	for (; h != NULL; h = h->next)
-	{
-		printf("%d\n", h->n);
-		nm++;
-	}
+	  {
+	    printf("%d\n", h->n);
+	    nm++;
+	  }
 
 	return (nm);
 }
