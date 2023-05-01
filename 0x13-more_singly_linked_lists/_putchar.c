06f4e4ff02e24a746c514f778e9f8b89efc47996
Everlyne <evahaudi2354@gmail.com>
2023-05-01 12:48:08 -0700

    putchar fumction

---
+++ 0x13-more_singly_linked_lists/_putchar.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * _putchar - writes a character to stdout
+ * @c: the character to print
+ *
+ * Return: On success 1, on error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+    return (putchar(c));
+}
