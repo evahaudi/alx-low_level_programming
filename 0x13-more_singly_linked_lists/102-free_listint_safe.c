efc6984b254d77aaf406b603cadce94df131471e
Everlyne <evahaudi2354@gmail.com>
2023-05-02 08:52:02 -0700

    updated

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,32 +1,34 @@
 #include "lists.h"
 
 /**
- * free_listint_safe - frees list safely
- * @head_ptr: pointer to pointer to head
+ * free_listint_safe - Frees a linked list safely
  *
- * Return: size of list freed
+ * @head_ptr: A pointer to a pointer to the head of the list
+ *
+ * Return: The size of the list that was freed
  */
 size_t free_listint_safe(listint_t **head_ptr)
 {
-    size_t count = 0;
-    listint_t *current_node, *next_node;
+	size_t count = 0;
+	listint_t *current_node, *next_node;
 
-    if (head_ptr == NULL || *head_ptr == NULL)
-        return (0);
+	if (head_ptr == NULL || *head_ptr == NULL)
+		return (0);
 
-    current_node = *head_ptr;
+	current_node = *head_ptr;
 
-    while (current_node != NULL)
-    {
-        next_node = current_node->next;
-        free(current_node);
-        count++;
+	while (current_node != NULL)
+	{
+		next_node = current_node->next;
+		free(current_node);
+		count++;
 
-        if (next_node >= current_node)
-	  break;
-	      current_node = next_node;
-    }
+		if (next_node >= current_node)
+			break;
 
-    *head_ptr = NULL;
-    return (count);
+		current_node = next_node;
+	}
+
+	*head_ptr = NULL;
+	return (count);
 }
