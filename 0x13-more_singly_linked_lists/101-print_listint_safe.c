90597951b8b901758e762cb52ba9f0cd6192cb6c
Everlyne <evahaudi2354@gmail.com>
2023-05-02 05:55:47 -0700

    updated

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,32 +1,37 @@
 #include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * print_listint_safe - Prints a linked list of integers
- * @head: Pointer to the head of the list
+ * print_listint_safe - Prints a linked list
+ *
+ * @head: pointer to the start of the list
  *
  * Return: The number of nodes in the list
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *current;
-	const listint_t *next;
-	size_t c = 0;
+	size_t no_of_nodes = 0;
 
 	if (head == NULL)
-		exit(98);
-
-	current = head;
-	while (current != NULL)
 	{
-		c++;
-		printf("[%p] %d\n", (void *)current, current->n);
-		next = current->next;
-		if (next >= current)
-		{
-			printf("-> [%p] %d\n", (void *)next, next->n);
-			break;
-		}
-		current = next;
+		fprintf(stderr, "Error: NULL pointer passed to print_listint_safe\n");
+		exit(98);
 	}
-	return (c);
+
+	while (head)
+	{
+		printf("[%p] %d\n", (void *)head, head->n);
+                no_of_nodes++;
+
+                if (head->next <= head)
+                {
+                        printf("-> [%p] %d\n", (void *)head->next, head->next->n);
+                        break;
+                }
+
+                head = head->next;
+	}
+
+	return (no_of_nodes);
 }
