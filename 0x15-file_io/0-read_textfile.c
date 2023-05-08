3542513d23946464e16f364e7bb047b068b11fc4
Everlyne <evahaudi2354@gmail.com>
2023-05-08 05:39:19 -0700

    updated

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,40 +1,37 @@
 #include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * read_textfile - Read text file print to STDOUT.
- * @filename: text file being read
- * @letters: number of letters to be read
- *
- * Return: actual number of bytes read and printed, or 0 if the function fails
- *         or filename is NULL.
+ * read_textfile - reads text file and writes to STDOUT
+ * @filename: name of file to read
+ * @letters: number of letters to read and write
+ * Return: number of bytes written to STDOUT, or 0 on failure
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    char *buf;
-    size_t n;
-    ssize_t w;
-    FILE *file;
+	FILE *fp;
+	char *buf;
+	size_t bytes_read, bytes_written;
 
-    if (!filename)
-        return (0);
+	if (!filename)
+		return (0);
 
-    file = fopen(filename, "r");
-    if (!file)
-        return (0);
+	fp = fopen(filename, "r");
+	if (!fp)
+		return (0);
 
-    buf = malloc(letters);
-    if (!buf)
-    {
-        fclose(file);
-        return (0);
-    }
+	buf = malloc(letters);
+	if (!buf) {
+		fclose(fp);
+		return (0);
+	}
 
-    n = fread(buf, sizeof(char), letters, file);
-    w = fwrite(buf, sizeof(char), n, stdout);
+	bytes_read = fread(buf, 1, letters, fp);
+	bytes_written = fwrite(buf, 1, bytes_read, stdout);
 
-    free(buf);
-    fclose(file);
+	free(buf);
+	fclose(fp);
 
-    return (w);
+	return (bytes_written);
 }
