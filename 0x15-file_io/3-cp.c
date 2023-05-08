188846179d90ecd6d653c271705f68a4a4f75201
Everlyne <evahaudi2354@gmail.com>
2023-05-08 09:24:29 -0700

    updated

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,54 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define BUFFER_SIZE 1024
+
+/**
+ * main - Copies the content of a file to another file.
+ *
+ * @argc: The number of arguments.
+ * @argv: The arguments.
+ *
+ * Return: If successful - 0.
+ *         Otherwise - 97 if wrong number of arguments,
+ *                     98 if can't read from file_from,
+ *                     99 if can't write to file_to,
+ *                     or 100 if can't close fd.
+ */
+int main(int argc, char *argv[])
+{
+int fd_from, fd_to, rd, wr;
+char buffer[BUFFER_SIZE];
+mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
+
+if (argc != 3)
+dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+
+fd_from = open(argv[1], O_RDONLY);
+if (fd_from == -1)
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
+
+fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, mode);
+if (fd_to == -1)
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+
+while ((rd = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+{
+wr = write(fd_to, buffer, rd);
+if (wr == -1)
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+}
+
+if (rd == -1)
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
+
+if (close(fd_from) == -1)
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from), exit(100);
+
+if (close(fd_to) == -1)
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to), exit(100);
+
+return (0);
+}
