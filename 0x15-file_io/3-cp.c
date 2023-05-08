4a6def9cff930561f211a3a1041aaa9e102c807d
Everlyne <evahaudi2354@gmail.com>
2023-05-08 09:06:07 -0700

    it is  a program that copies the content of a file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,56 +3,56 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-#define BUFFER_SIZE 1024
+#define MEMORY_SIZE 1024
 
 /**
- * error - Prints an error message to stderr and exits with a non-zero status.
- * @msg: The error message to print.
+ * error - prints an error message to stderr and exits with a non-zero status.
+ * @m: the error message to print.
  */
-void error(char *msg)
+void error(char *m)
 {
-    fprintf(stderr, "%s", msg);
+    fprintf(stderr, "%s", m);
     exit(1);
 }
 
 /**
- * main - Copies the contents of one file to another file.
- * @argc: The number of arguments.
- * @argv: The arguments.
+ * main - copies the contents of one file to another file.
+ * @argc: the number of arguments.
+ * @argv: the arguments.
  *
  * Return: 0 on success.
  */
 int main(int argc, char *argv[])
 {
-    int input_fd, output_fd;
-    ssize_t bytes_read, bytes_written;
-    char buffer[BUFFER_SIZE];
+    int file1, file2;
+    ssize_t byts_read, byts_written;
+    char buffer[MEMORY_SIZE];
 
     if (argc != 3)
         error("Usage: cp file1 file2\n");
 
-    input_fd = open(argv[1], O_RDONLY);
-    if (input_fd == -1)
+    file1 = open(argv[1], O_RDONLY);
+    if (file1 == -1)
         error("Error: Can't read from file1\n");
 
-    output_fd = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0644);
-    if (output_fd == -1)
+    file2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0644);
+    if (file2 == -1)
         error("Error: Can't write to file2\n");
 
-    while ((bytes_read = read(input_fd, buffer, BUFFER_SIZE)) > 0)
+    while ((byts_read = read(file1, buffer, BUFFER_SIZE)) > 0)
     {
-        bytes_written = write(output_fd, buffer, bytes_read);
-        if (bytes_written != bytes_read)
+        byts_written = write(file2, buffer, byts_read);
+        if (byts_written != byts_read)
             error("Error: Can't write to file2\n");
     }
 
-    if (bytes_read == -1)
+    if (byts_read == -1)
         error("Error: Can't read from file1\n");
 
-    if (close(input_fd) == -1)
+    if (close(file1) == -1)
         error("Error: Can't close file1\n");
 
-    if (close(output_fd) == -1)
+    if (close(file2) == -1)
         error("Error: Can't close file2\n");
 
     return 0;
