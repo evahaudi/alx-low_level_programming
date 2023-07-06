18e56b7241d16fbfd7ac80de7619c9f7c29fe0e4
EVERLYNE AKINYI <evahaudi2354@gmail.com>
2023-07-06 13:10:33 +0300

    updated

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,7 +11,7 @@ unsigned int binary_to_uint(const char *b) {
     unsigned int result = 0;
 
     if (b == NULL)
-        return 0;
+        return (0);
 
     while (*b) {
         if (*b != '0' && *b != '1')
@@ -21,5 +21,5 @@ unsigned int binary_to_uint(const char *b) {
         b++;
     }
 
-    return result;
+    return (result);
 }
