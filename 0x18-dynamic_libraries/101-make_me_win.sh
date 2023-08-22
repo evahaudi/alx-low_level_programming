221e2da7d9d5fa304e0840ce26f6fa5e812a66d0
Everlyne Akinyi <evahaudi2354@gmail.com>
2023-08-22 07:13:33 -0700

    updated

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/evahaudi/alx-low_level_programming/blob/master/0x18-dynamic_libraries/if_win.so
-export LD_PRELOAD=/tmp/if_win.so
+wget -P /tmp https://github.com/evahaudi/alx-low_level_programming/blob/master/0x18-dynamic_libraries/iwin.so
+export LD_PRELOAD=/tmp/iwin.so
